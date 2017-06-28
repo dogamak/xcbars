d50f7f10e0ec7002055635400afe2966579e1fea
Mitja Karhusaari <monttu300@gmail.com>
2017-06-28 22:44:38 +0300

    Rewrite and split the Component trait.
    The trait `Component` is now split into `ComponentConfig` ans
    `ComponentState`, which allows us more flexibility with the
    user facing configuration types. (Eg. accepting a `&str` and copying it
    into a `String` afterwards)
    
    The `ComponentState` has now more methods than the previous
    `Component` trait had, which allows for better error messages. The
    `render` method also accepts a `cairo::Surface` so that components can
    render arbitary visuals on the bar.

--- src/component.rs
+++ src/component.rs
@@ -1,48 +1,147 @@
 use futures::Stream;
-use error::*;
-use std::result::Result as StdResult;
 use tokio_core::reactor::Handle;
-use components::Text;
+use cairo::Surface;
+use std::mem;
+use std::error::Error as StdError;
+use std::result::Result as StdResult;
+use error::*;
+use std::any::Any;
+// use components::Text;
 
-pub trait Component {
-    type Stream: Stream<Item = String, Error = Self::Error> + 'static;
-    type Error: ::std::error::Error + Send + 'static;
+pub trait ComponentConfig: Sized {
+    type State: ComponentState<Config=Self>;
+}
 
-    fn init(&mut self) -> StdResult<(), Self::Error> {
+pub trait ComponentState: Stream + Sized {
+    type Config: ComponentConfig<State=Self>;
+
+    fn create(config: Box<Self::Config>, handle: &Handle) -> StdResult<Self, Self::Error>;
+    fn get_preferred_width(&self) -> u16;
+    fn update(&mut self, update: Box<Self::Item>) -> StdResult<(), Self::Error>;
+    fn render(&self, surface: Surface) -> StdResult<(), Self::Error>;
+}
+
+enum ComponentWrapper<C: ComponentConfig> {
+    Config(C),
+    State(C::State),
+    Tmp,
+}
+
+pub trait ComponentConfigExt {
+    fn create(
+        self: Box<Self>,
+        handle: &Handle,
+    ) -> Result<Box<ComponentStateExt>>;
+}
+
+impl<C> ComponentConfigExt for C
+    where C: ComponentConfig,
+          <C as ComponentConfig>::State: 'static,
+          <<C as ComponentConfig>::State as Stream>::Error: StdError + Send + 'static,
+{
+    fn create(
+        self: Box<Self>,
+        handle: &Handle,
+    ) -> Result<Box<ComponentStateExt>> {
+        let state = C::State::create(self, handle)
+            .map_err(|e| Error::with_chain(e, "Failed to create component"))?;
+        Ok(Box::new(state))
+    }
+}
+
+pub trait ComponentStateExt {
+    fn get_preferred_width(&self) -> u16;
+    fn update(&mut self, update: Box<Any>) -> Result<()>;
+    fn render(&self, surface: Surface) -> Result<()>;
+}
+
+impl<C> ComponentStateExt for C
+    where C: ComponentState,
+<C as Stream>::Error: StdError + Send + 'static,
+<C as Stream>::Item: 'static,
+{
+    fn get_preferred_width(&self) -> u16 {
+        ComponentState::get_preferred_width(self)
+    } 
+
+    fn update(&mut self, update: Box<Any>) -> Result<()> {
+        if let Ok(update) = update.downcast() {
+            Ok(ComponentState::update(self, update)
+               .map_err(|e| Error::with_chain(e, "Failed to update component"))?)
+        } else {
+            Ok(())
+        }
+    }
+
+    fn render(&self, surface: Surface) -> Result<()> {
+        Ok(ComponentState::render(self, surface)
+           .map_err(|e| Error::with_chain(e, "Failed to render component"))?)
+    }
+}
+
+trait IntoBoxedComponentWrapperExt {
+    fn into(self) -> Box<ComponentWrapperExt>;
+}
+
+impl<C> IntoBoxedComponentWrapperExt for C
+    where C: ComponentConfig + 'static,
+          <<C as ComponentConfig>::State as Stream>::Error: StdError + Send,
+{
+    fn into(self) -> Box<ComponentWrapperExt> {
+        Box::new(ComponentWrapper::Config(self))
+    } 
+}
+
+pub trait ComponentWrapperExt {
+    fn create(&mut self, handle: &Handle) -> Result<()>;
+    fn get_preferred_width(&self) -> u16;
+    fn update(&mut self, update: Box<Any>) -> Result<()>;
+    fn render(&self, surface: Surface) -> Result<()>;
+}
+
+impl<C> ComponentWrapperExt for ComponentWrapper<C>
+    where C: ComponentConfig,
+          <<C as ComponentConfig>::State as Stream>::Error: StdError + Send + 'static,
+<<C as ComponentConfig>::State as Stream>::Item: 'static,
+{
+    fn create(&mut self, handle: &Handle) -> Result<()> {
+        let swaped = mem::replace(self, ComponentWrapper::Tmp);
+        if let ComponentWrapper::Config(conf) = swaped {
+            let state = C::State::create(Box::new(conf), handle)
+                .map_err(|e| Error::with_chain(e, "Failed to create component"))?;
+            mem::replace(self, ComponentWrapper::State(state));
+        }
         Ok(())
     }
-    fn stream(self, handle: Handle) -> Self::Stream;
-}
 
-pub trait ComponentCreator {
-    fn init(&mut self) -> StdResult<(), Error>;
-    fn into_stream(self: Box<Self>, handle: Handle) -> Box<Stream<Item = String, Error = Error>>;
-    fn create(
-        mut self: Box<Self>,
-        handle: Handle,
-    ) -> StdResult<Box<Stream<Item = String, Error = Error>>, Error> {
-        self.init()?;
-        Ok(self.into_stream(handle))
+    fn update(&mut self, update: Box<Any>) -> Result<()> {
+        if let ComponentWrapper::State(ref mut conf) = *self {
+            if let Ok(update) = update.downcast() {
+                ComponentState::update(conf, update)
+                    .map_err(|e| Error::with_chain(e, "Failed to update component"))?;
+            }
+        }
+        Ok(())
+    }
+
+    fn render(&self, surface: Surface) -> Result<()> {
+        if let ComponentWrapper::State(ref conf) = *self {
+            ComponentState::render(conf, surface)
+                .map_err(|e| Error::with_chain(e, "Failed to render component"))?;
+        }
+        Ok(())
+    }
+
+    fn get_preferred_width(&self) -> u16 {
+        if let ComponentWrapper::State(ref conf) = *self {
+            ComponentState::get_preferred_width(conf)
+        } else {
+            0
+        }
     }
 }
 
-impl<C> ComponentCreator for C
-where
-    C: Component,
-{
-    fn init(&mut self) -> StdResult<(), Error> {
-        Component::init(self).chain_err(|| "Failed to initialize component")
-    }
-
-    fn into_stream(self: Box<Self>, handle: Handle) -> Box<Stream<Item = String, Error = Error>> {
-        Box::new(
-            self.stream(handle)
-                .map_err(|e| Error::with_chain(e, "Component raised an error")),
-        )
-    }
-}
-
-pub struct SubComponent(pub Box<ComponentCreator>);
+/* pub struct SubComponent(pub Box<ComponentCreator>);
 
 impl<'s> From<&'s str> for SubComponent {
     fn from(s: &'s str) -> SubComponent {
@@ -57,7 +156,7 @@ where
     fn from(c: C) -> SubComponent {
         SubComponent(Box::new(c))
     }
-}
+} */
 
 #[derive(Clone, Copy, Debug)]
 pub enum Slot {
