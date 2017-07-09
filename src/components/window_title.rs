845e22d9086714a85467b8e3e84b0ba9d6e9ac0d
Christian Dürr <contact@christianduerr.com>
2017-07-09 23:28:23 +0200

    Add support for EWMH window title
    Now always first checks `_NET_WM_NAME` before falling back to `WM_NAME`.
    This might be a bit inefficient but seems to work well.

--- src/components/window_title.rs
+++ src/components/window_title.rs
@@ -1,6 +1,6 @@
 use component::Component;
 use tokio_core::reactor::Handle;
-use error::Error;
+use error::{Error, Result, ErrorKind};
 use xcb::{self, Connection};
 use xcb_event_stream;
 use futures::stream::Stream;
@@ -11,28 +11,39 @@ const ATOM_FOCUS_CHANGE: xcb::AtomEnum = 334;
 pub struct WindowTitle {}
 
 // Get the title of the window that currently is focused
-fn get_window_title() -> String {
+fn get_window_title() -> Result<String> {
     // Connect to Xorg
-    let (conn, _) = Connection::connect(None).unwrap();
+    let (conn, _) = Connection::connect(None)
+        .map_err(|e| ErrorKind::XcbConnection(e))?;
 
     // Get the window that is currently in focus
     let input_focus_cookie = xcb::get_input_focus(&conn);
-    let window = input_focus_cookie.get_reply().unwrap().focus();
+    let window = input_focus_cookie.get_reply()?.focus();
 
     // Use ewmh to poll for the _NET_WM_NAME of the focused window
-    let property_cookie = xcb::get_property(
-        &conn,
-        false,
-        window,
-        xcb::ATOM_WM_NAME,
-        xcb::ATOM_STRING,
-        0,
-        32,
-    );
-    let property_reply = property_cookie.get_reply().unwrap();
-    let name = String::from_utf8_lossy(property_reply.value());
+    let atom = xcb::intern_atom(&conn, true, "_NET_WM_NAME")
+        .get_reply()?
+        .atom();
 
-    name.into()
+    let property_reply = xcb::get_property(&conn, false, window, atom, xcb::ATOM_ANY, 0, 32)
+        .get_reply();
+    let reply = match property_reply {
+        Ok(reply) => reply,
+        _ => {
+            xcb::get_property(
+                &conn,
+                false,
+                window,
+                xcb::ATOM_WM_NAME,
+                xcb::ATOM_STRING,
+                0,
+                32,
+            ).get_reply()?
+        }
+    };
+
+    let name = String::from_utf8_lossy(reply.value());
+    Ok(name.into())
 }
 
 // Poll the window title on a timer
@@ -61,7 +72,7 @@ impl Component for WindowTitle {
                 let property_atom = property_event.atom();
                 property_atom == ATOM_FOCUS_CHANGE
             })
-            .and_then(move |_| Ok(get_window_title()));
+            .and_then(move |_| get_window_title());
 
         Box::new(stream)
     }
