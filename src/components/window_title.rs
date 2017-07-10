30665c492444377387a0e22bed5e881ce5dcbbf5
Christian Dürr <contact@christianduerr.com>
2017-07-10 22:29:31 +0200

    Change to support all EWMH WMs
    This change acquires the EWMH atoms dynamically. This should make it
    work on all WMs that support EWMH.
    
    If EWMH is not supported this will fail with an error after the first
    event is received.

--- src/components/window_title.rs
+++ src/components/window_title.rs
@@ -5,44 +5,76 @@ use xcb::{self, Connection};
 use xcb_event_stream;
 use futures::stream::Stream;
 
-const ATOM_FOCUS_CHANGE: xcb::AtomEnum = 334;
-
 /// This struct is used for creation and storing the refresh rate.
-pub struct WindowTitle;
+pub struct WindowTitle {
+    active_window: xcb::AtomEnum,
+    wm_name: xcb::AtomEnum,
+}
+
+impl WindowTitle {
+    pub fn new() -> Result<WindowTitle> {
+        let (conn, _) = Connection::connect(None)
+            .map_err(|e| ErrorKind::XcbConnection(e))?;
+
+        let active_window = xcb::intern_atom(&conn, true, "_NET_ACTIVE_WINDOW")
+            .get_reply()?
+            .atom();
+        let wm_name = xcb::intern_atom(&conn, true, "_NET_WM_NAME")
+            .get_reply()?
+            .atom();
+
+        Ok(WindowTitle {
+            active_window,
+            wm_name,
+        })
+    }
+}
 
 // Get the title of the window that currently is focused
-fn get_window_title() -> Result<String> {
+fn get_window_title(window_title: &WindowTitle) -> Result<String> {
     // Connect to Xorg
-    let (conn, _) = Connection::connect(None)
+    let (conn, screen_num) = Connection::connect(None)
         .map_err(|e| ErrorKind::XcbConnection(e))?;
 
-    // Get the window that is currently in focus
-    let input_focus_cookie = xcb::get_input_focus(&conn);
-    let window = input_focus_cookie.get_reply()?.focus();
+    // Get the screen for accessing the root window later
+    let setup = conn.get_setup();
+    let screen = setup
+        .roots()
+        .nth(screen_num as usize)
+        .ok_or("Unable to acquire screen.")?;
 
-    // Use ewmh to poll for the _NET_WM_NAME of the focused window
-    let atom = xcb::intern_atom(&conn, true, "_NET_WM_NAME")
-        .get_reply()?
-        .atom();
+    // Get the currently active window
+    let property_reply = xcb::get_property(
+        &conn,
+        false,
+        screen.root(),
+        window_title.active_window,
+        xcb::ATOM_WINDOW,
+        0,
+        32,
+    ).get_reply()?;
 
-    let property_reply = xcb::get_property(&conn, false, window, atom, xcb::ATOM_ANY, 0, 32)
-        .get_reply();
-    let reply = match property_reply {
-        Ok(reply) => reply,
-        _ => {
-            xcb::get_property(
-                &conn,
-                false,
-                window,
-                xcb::ATOM_WM_NAME,
-                xcb::ATOM_STRING,
-                0,
-                32,
-            ).get_reply()?
-        }
-    };
+    // Get the window from the reply
+    // Returns with error if no window has been found
+    let active_window_value: &[u32] = property_reply.value();
+    if active_window_value.is_empty() {
+        return Err("Could not get active window.".into());
+    }
+    let window = active_window_value[0];
 
-    let name = String::from_utf8_lossy(reply.value());
+    // Get the title of the active window
+    let property_reply = xcb::get_property(
+        &conn,
+        false,
+        window,
+        window_title.wm_name,
+        xcb::ATOM_ANY,
+        0,
+        32,
+    ).get_reply()?;
+
+    // Convert active window reply to string and return it
+    let name = String::from_utf8_lossy(property_reply.value());
     Ok(name.into())
 }
 
@@ -54,6 +86,7 @@ impl Component for WindowTitle {
     fn stream(self, _: Handle) -> Self::Stream {
         let (conn, screen_num) = Connection::connect(None).unwrap();
 
+        // Setup the event for the root screen
         {
             let setup = conn.get_setup();
             let screen = setup.roots().nth(screen_num as usize).unwrap();
@@ -65,14 +98,17 @@ impl Component for WindowTitle {
             );
         }
 
+        // Start event receiver future
+        // Executes `get_window_title` every time window title event is received
         conn.flush();
+        let active_window = self.active_window;
         let stream = xcb_event_stream::XcbEventStream::new(conn)
-            .filter(|event| unsafe {
+            .filter(move |event| unsafe {
                 let property_event: &xcb::PropertyNotifyEvent = xcb::cast_event(&event);
                 let property_atom = property_event.atom();
-                property_atom == ATOM_FOCUS_CHANGE
+                property_atom == active_window
             })
-            .and_then(move |_| get_window_title());
+            .and_then(move |_| get_window_title(&self));
 
         Box::new(stream)
     }
