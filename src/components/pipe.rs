b04b74428b54bff822dfb015315dc71d21e17adb
Mitja Karhusaari <monttu300@gmail.com>
2017-07-14 17:41:32 +0300

    Implement `components::Pipe` using `StringComponent`.

--- src/components/pipe.rs
+++ src/components/pipe.rs
@@ -1,13 +1,16 @@
-use component::Component;
+use string_component::StringComponent;
+use bar::BarInfo;
+use std::rc::Rc;
 use tokio_core::reactor::Handle;
 use std::io::BufReader;
 use std::process::{Stdio, Command};
 use tokio_process::{ChildStdout, CommandExt};
-use futures::{Stream, Future};
-use tokio_timer::Timer;
+use futures::{Stream, Future, Poll, Async};
+use tokio_timer::{Sleep, Timer};
 use std::time::Duration;
 use utils::LoopFn;
-use error::Error;
+use error::{Result, Error};
+use tokio_io::io::Lines;
 
 pub struct Pipe {
     pub command: String,
@@ -25,44 +28,93 @@ impl Default for Pipe {
     }
 }
 
-impl Pipe {
-    fn reader(&self, handle: &Handle) -> BufReader<ChildStdout> {
-        let mut cmd = Command::new(self.command.as_str());
-        cmd.args(self.args.as_slice());
-        cmd.stdin(Stdio::inherit()).stdout(Stdio::piped());
-        let mut child = cmd.spawn_async(&handle).unwrap();
-        let stdout = child.stdout().take().unwrap();
-        handle.spawn(child.map(|_| ()).map_err(|_| ()));
-        BufReader::new(stdout)
+enum PipeStreamState {
+    Command(Lines<BufReader<ChildStdout>>),
+    Sleep(Sleep),
+}
+
+struct PipeStream {
+    handle: Handle,
+    config: Pipe,
+    state: PipeStreamState,
+    timer: Timer,
+}
+
+fn command_lines(config: &Pipe, handle: &Handle) -> Lines<BufReader<ChildStdout>> {
+    let mut cmd = Command::new(config.command.as_str());
+    cmd.args(config.args.as_slice());
+    cmd.stdin(Stdio::inherit()).stdout(Stdio::piped());
+    let mut child = cmd.spawn_async(&handle).unwrap();
+    let stdout = child.stdout().take().unwrap();
+    handle.spawn(child.map(|_| ()).map_err(|_| ()));
+    ::tokio_io::io::lines(BufReader::new(stdout))
+}
+
+impl PipeStream {
+    fn new<'a>(config: Pipe, handle: Handle) -> PipeStream {
+        PipeStream {
+            state: PipeStreamState::Command(command_lines(&config, &handle)),
+            handle: handle,
+            config: config,
+            timer: Default::default(),
+        }
     }
 }
 
-impl Component for Pipe {
+impl Stream for PipeStream {
+    type Item = String;
     type Error = Error;
-    type Stream = Box<Stream<Item = String, Error = Error>>;
 
-    fn stream(self, handle: Handle) -> Self::Stream {
-        if let Some(refresh_rate) = self.refresh_rate {
-            Box::new(
-                LoopFn::new(move || {
-                    let timer = Timer::default();
-                    Ok::<_, Error>(
-                        ::tokio_io::io::lines(self.reader(&handle))
-                            .map(|s| Some(s))
-                            .chain(
-                                timer
-                                    .sleep(refresh_rate)
-                                    .into_stream()
-                                    .map(|_| None)
-                                    .from_err(),
-                            )
-                            .map_err(|e| Error::from(e)),
-                    )
-                }).flatten()
-                    .filter_map(|s| s),
-            )
-        } else {
-            Box::new(::tokio_io::io::lines(self.reader(&handle)).from_err())
+    fn poll(&mut self) -> Poll<Option<String>, Error> {
+        let mut line_received = None;
+        let mut sleep_done = false;
+        
+        match self.state {
+            PipeStreamState::Command(ref mut lines) => {
+                match lines.poll() {
+                    Ok(Async::NotReady) => return Ok(Async::NotReady),
+                    Err(e) => return Err(e.into()),
+                    Ok(Async::Ready(Some(line))) => {
+                        line_received = Some(line);
+                    },
+                    _ => {},
+                }
+            },
+            PipeStreamState::Sleep(ref mut future) => {
+                match future.poll() {
+                    Ok(Async::NotReady) => return Ok(Async::NotReady),
+                    Err(e) => return Err(e.into()),
+                    Ok(Async::Ready(_)) => {
+                        sleep_done = true;
+                    },
+                }
+            },
         }
+
+        println!("{:?} {:?}", line_received, sleep_done);
+
+        if let Some(line) = line_received {
+            if let Some(duration) = self.config.refresh_rate {
+                self.state = PipeStreamState::Sleep(self.timer.sleep(duration));
+            }
+
+            return Ok(Async::Ready(Some(line)));
+        }
+
+        if sleep_done {
+            self.state = PipeStreamState::Command(command_lines(&self.config, &self.handle));
+            return Ok(Async::NotReady);
+        }
+
+        unreachable!();
+    }
+}
+
+impl StringComponent for Pipe {
+    type Error = Error;
+    type Stream = PipeStream;
+
+    fn create(config: Self, _: Rc<BarInfo>, handle: &Handle) -> Result<Self::Stream> {
+        Ok(PipeStream::new(config, handle.clone()))
     }
 }
