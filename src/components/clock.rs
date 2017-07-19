31d437c3733485c249cee31274618a25616bfe3a
Christian Dürr <contact@christianduerr.com>
2017-07-19 18:51:49 +0200

    Refactor `get_refresh_rate`
    The `get_refresh_rate` method now converts a known time to a String and
    back to a time to determine the accuracy of the specified `strftime`
    format. This allows it to keep full `strftime` syntax compatibility
    without breaking `%%` to escape `%`.

--- src/components/clock.rs
+++ src/components/clock.rs
@@ -1,15 +1,14 @@
 use std::time::{Duration, Instant};
 use tokio_core::reactor::Handle;
+use error::{Error, Result};
 use component::Component;
 use tokio_timer::Timer;
 use futures::Stream;
-use error::Error;
 use time;
 
 /// A `Clock` that automatically determines the refresh rate.
 ///
-/// This uses the default [strftime](https://linux.die.net/man/3/strftime) syntax for formatting.
-/// Escaping `%` using `%%` is however not supported.
+/// This uses the default [strftime](http://man7.org/linux/man-pages/man3/strftime.3.html) syntax for formatting.
 pub struct Clock {
     format: String,
     refresh_rate: Duration,
@@ -30,25 +29,39 @@ impl Default for Clock {
 
 impl Clock {
     /// Create a new `Clock` specifying the time format as argument.
-    pub fn new<T: Into<String>>(format: T) -> Clock {
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the specified `strftime` format could not be parsed.
+    pub fn new<T: Into<String>>(format: T) -> Result<Clock> {
         let format = format.into();
-        let refresh_rate = get_refresh_rate(&format);
-        Clock {
+        let refresh_rate = get_refresh_rate(&format)?;
+        Ok(Clock {
             format: format,
             refresh_rate,
-        }
+        })
     }
 }
 
 // Checks if seconds are part of the `strftime` format.
 // If there are no seconds the refresh rate is one minute, otherwise it's one second.
-fn get_refresh_rate(format: &str) -> Duration {
-    for i in &["%c", "%r", "%S", "%T", "%X"] {
-        if format.contains(i) {
-            return Duration::from_secs(1);
-        }
+fn get_refresh_rate(format: &str) -> Result<Duration> {
+    // Create a time with any non-zero second value
+    let mut time = time::now();
+    time.tm_sec = 15;
+
+    // Convert to String and back to Tm
+    let time_str = time::strftime(format, &time)
+        .map_err(|_| "Invalid clock format.")?;
+    let time = time::strptime(&time_str, format)
+        .map_err(|_| "Invalid clock format.")?;
+
+    // Check if seconds are still there
+    if time.tm_sec != 0 {
+        Ok(Duration::from_secs(1))
+    } else {
+        Ok(Duration::from_secs(60))
     }
-    Duration::from_secs(60)
 }
 
 impl Component for Clock {
